33ce6fcce8bd9f271626f3448f535221f05343df
SetyTona <setytona@gmail.com>
2021-02-14 07:45:07 +0100

    RESTServer Subida Inicial API 3

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
     constructor() {
 
         this.app = express();
-        this.port = process.env.port;
+        this.port = process.env.PORT;
 
         // Middlewares
         this.middlewares();
